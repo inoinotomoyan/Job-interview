e4ada5cae22a3835e1a57e692fdaccbb5733c267
inoinotomoyan <i09243475inochan@outlook.jp>
2024-08-31 15:17:19 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const links = document.querySelectorAll('a[href^="#toppage"]');
+//   const offset = 50;
+
+//   links.forEach((link) => {
+//     link.addEventListener("click", function (event) {
+//       event.preventDefault();
+
+//       const targetId = this.getAttribute("href").substring(1);
+//       const targetElement = document.getElementById(targetId);
+
+//       if (targetElement) {
+//         window.scrollTo({
+//           top: targetElement.offsetTop - offset,
+//           behavior: "smooth",
+//         });
+//       }
+//     });
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+  const offset = 50;
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - offset,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
